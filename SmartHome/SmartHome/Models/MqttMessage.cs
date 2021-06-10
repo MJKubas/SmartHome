59fd988f286daa1feac98df136c4b65c131aaa37
Michał <kubas.m.j@gmail.com>
2021-06-10 16:34:09 +0200

    changes after meeting

---
+++ SmartHome/SmartHome/Models/MqttMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHome.Models
+{
+    class MqttMessage
+    {
+        public SensorDummy Device { get; set; }
+        public string AuthToken { get; set; }
+        public string MqttType { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ SmartHome/SmartHome/Models/SensorDummy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHome.Models
+{
+    class SensorDummy
+    {
+        public string Type { get; set; }
+        public string Topic { get; set; }
+        public string View { get; set; }
+    }
+}
